fd443db228c4e93f28fc2cde1f8a7f500cf1968f
Antonio Giroz <antonioluisgil@gmail.com>
2025-01-04 17:55:51 +0100

    feat: add file based router

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
 import antfu from '@antfu/eslint-config';
+import pluginRouter from '@tanstack/eslint-plugin-router';
 
 export default antfu({
   react: true,
   stylistic: {
     semi: true,
   },
+  ignores: ['**/routeTree.gen.ts'],
+  plugins: [pluginRouter],
 });
--- src/app.tsx
+++
@@ -1,9 +0,0 @@
-export function App() {
-  return (
-    <>
-      <div className="container mx-auto">
-        <h1 className="font-bold text-3xl">Demo</h1>
-      </div>
-    </>
-  );
-}
