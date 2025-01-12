6a2102cb9ae9577d5939b167cf2f689778fabe46
Antonio Giroz <antonioluisgil@gmail.com>
2025-01-12 11:24:18 +0100

    chore: disable router dev tools in prod

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -2,11 +2,19 @@ import { Toaster } from '@/components/ui/sonner';
 import { SheetProvider } from '@/providers/sheet-provider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { lazy } from 'react';
 
 export const Route = createRootRoute({
   component: Root,
 });
+
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null
+  : lazy(() =>
+      import('@tanstack/router-devtools').then(res => ({
+        default: res.TanStackRouterDevtools,
+      })),
+    );
 
 function Root() {
   const queryClient = new QueryClient();
